ec1ed4726d41d5bd99afb5f41c833c07c30cf721
Bob B <bpblinde@live.nl>
2023-07-13 19:49:53 -0600

    Added functions to move the magnet

--- src/boardMover.py
+++ src/boardMover.py
@@ -2,14 +2,32 @@ import serial, time
 
 class BoardMover:
     #BoardMover will make movement using GCODE based on chess moves
-    x_max = 185
-    y_max = 170
+    X_MAX = 185
+    Y_MAX = 170
+    MAGNET_PIN = "P44"
 
-    def __init__(self, port, baudrate=115200):
-        self.serialPort = serial.Serial(port = port, baudrate = baudrate)
-        time.sleep(1)
-        self.serialPort.write(str.encode("G28\r\n"))
 
+    def __init__(self, port, baudrate=115200, test=False):
+        if not test:
+            self.serialPort = serial.Serial(port = port, baudrate = baudrate)
+            time.sleep(1)
+            self.send_gcode("G28")
+            #self.serialPort.write(str.encode("G28\r\n"))
+            time.sleep(1)
+            self.x = 0
+            self.y = 0
+
+    def move_relative(self, x, y):
+        self.x += x
+        self.y += y
+        self.send_gcode("G0 X" + str(self.x) + " Y" + str(self.y))
+        return
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        self.send_gcode("G0 X" + str(self.x) + " Y" + str(self.y))
+        return
+    
     def move_piece(self, origin, destination):
         return
     
@@ -17,8 +35,21 @@ class BoardMover:
         return
     
     def send_gcode(self, gcode):
-        self.serialPort.write(str.encode(gcode + "\r\n"))
+        self.serialPort.write((gcode + "\n").encode())
+        time.sleep(1)
         return
 
-board = BoardMover("COM8")
-board.send_gcode("G1 X185 Y170")
+    def start_magnet(self):
+        self.send_gcode("M42 " + self.MAGNET_PIN + " S255")
+        return
+    def stop_magnet(self):
+        self.send_gcode("M42 " + self.MAGNET_PIN + " S0")
+        return
+    
+    def board_to_coord(self, square):
+        x =  160 - (ord(square[0]) - 65) *20
+        y = int(square[1]) * 20
+        return x, y
+    
+    def coord_to_board(self, coord):
+        return
